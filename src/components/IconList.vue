ce2c0bfd5a337800f697532137739fcedef27a51
独断万古丶荒 <411496621@qq.com>
2025-02-06 15:21:38 +0800

    iconList组件进行中 渲染组件列表完成 todo 加上对应的点击事件功能

--- src/components/IconList.vue
+++ src/components/IconList.vue
@@ -1,19 +1,32 @@
 <template>
-  <div>
-    <component
-      style="display: inline-block"
-      :is="Icon"
-      :icon="'ep:' + item"
+  <ul class="flex flex-wrap border rounded">
+    <li
+      class="w-1/8 border-r border-b flex flex-col items-center justify-center cursor-pointer hover:bg-sky-100 py-4"
       v-for="(item, index) in json"
       :key="index"
     >
-    </component>
-  </div>
+      <component class="text-3xl mb-3" :is="Icon" :icon="'ep:' + item"> </component>
+
+      <div class="text-sm">
+        {{ toPascalCase(item) }}
+      </div>
+    </li>
+  </ul>
 </template>
 
 <script setup lang="ts">
 import json from './icon-ep.json'
 import { Icon, loadIcons } from '@iconify/vue'
+
+function toPascalCase(input: string): string {
+  return input
+    .split('-') // 按照连字符分割字符串
+    .map((word) => {
+      // 将每个子字符串的首字母大写
+      return word.charAt(0).toUpperCase() + word.slice(1)
+    })
+    .join('') // 拼接成一个新的字符串
+}
 
 onBeforeMount(async () => {
   await loadIcons(json)
