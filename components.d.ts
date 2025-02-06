26ef6470922a50e2945439521e208d6181ffbc05
独断万古丶荒 <411496621@qq.com>
2025-02-06 15:43:57 +0800

    iconList组件基本完成

--- components.d.ts
+++ components.d.ts
@@ -9,6 +9,7 @@ declare module 'vue' {
   export interface GlobalComponents {
     ElButton: typeof import('element-plus/es')['ElButton']
     ElRow: typeof import('element-plus/es')['ElRow']
+    ElSwitch: typeof import('element-plus/es')['ElSwitch']
     IconfontIcon: typeof import('./src/components/IconfontIcon.vue')['default']
     IconList: typeof import('./src/components/IconList.vue')['default']
     NetIcon: typeof import('./src/components/NetIcon.vue')['default']
--- src/components/IconList.vue
+++ src/components/IconList.vue
@@ -1,9 +1,14 @@
 <template>
+  <div>
+    <el-switch v-model="copyTypeFlag" active-text="复制icon名称" inactive-text="复制SVG图标" />
+  </div>
+
   <ul class="flex flex-wrap border rounded">
     <li
       class="w-1/8 border-r border-b flex flex-col items-center justify-center cursor-pointer hover:bg-sky-100 py-4"
       v-for="(item, index) in json"
       :key="index"
+      @click="handleClick(item)"
     >
       <component class="text-3xl mb-3" :is="Icon" :icon="'ep:' + item"> </component>
 
@@ -16,7 +21,23 @@
 
 <script setup lang="ts">
 import json from './icon-ep.json'
-import { Icon, loadIcons } from '@iconify/vue'
+import { Icon, loadIcons, loadIcon } from '@iconify/vue'
+import { ElMessage } from 'element-plus'
+
+// false->copySvgName   true->copyName
+const copyTypeFlag = ref(true)
+const source = ref('')
+const { copy, copied } = useClipboard({ source })
+
+function objectToSvg(svgObject: any) {
+  const { body, hFlip, height, left, rotate, top, vFlip, width } = svgObject
+  const svgString = `<svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}" transform="translate(${left},${top}) rotate(${rotate})${
+    hFlip ? ' scale(-1, 1)' : ''
+  }${vFlip ? ' scale(1, -1)' : ''}">
+        ${body}
+    </svg>`
+  return svgString
+}
 
 function toPascalCase(input: string): string {
   return input
@@ -26,6 +47,20 @@ function toPascalCase(input: string): string {
       return word.charAt(0).toUpperCase() + word.slice(1)
     })
     .join('') // 拼接成一个新的字符串
+}
+
+async function handleClick(icon: string) {
+  if (copyTypeFlag.value) {
+    source.value = toPascalCase(icon)
+  } else {
+    const res = await loadIcon('ep:' + icon)
+    source.value = objectToSvg(res)
+  }
+  await copy()
+  ElMessage({
+    message: '复制成功',
+    type: 'success'
+  })
 }
 
 onBeforeMount(async () => {
