6f51071375295181dd31e3f9d5c450ed489257b8
独断万古丶荒 <411496621@qq.com>
2025-02-07 10:56:18 +0800

    集成i18n基本使用

--- src/main.ts
+++ src/main.ts
@@ -10,14 +10,30 @@ import './assets/main.css'
 import '@unocss/reset/tailwind.css'
 import 'virtual:uno.css'
 import 'virtual:svg-icons-register'
-import "element-plus/dist/index.css"
+import 'element-plus/dist/index.css'
 
 import 'element-plus/theme-chalk/dark/css-vars.css'
+import { createI18n } from 'vue-i18n'
 
 const app = createApp(App)
+
+const i18n = createI18n({
+  legacy: false,
+  locale: 'zh',
+  fallbackLocale: 'en',
+  messages: {
+    en: {
+      hello: 'hello world'
+    },
+    zh: {
+      hello: '你好世界'
+    }
+  }
+})
+
+app.use(i18n)
 
 app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
-
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,15 +1,26 @@
 <template>
   <el-row class="mb-4">
-    <DarkModeToggle :dark="true" > </DarkModeToggle>
+    <DarkModeToggle :dark="true"> </DarkModeToggle>
     <FullScreen></FullScreen>
   </el-row>
 
   <div class="w-25 h-25 bg-red dark:bg-sky">块元素</div>
+
+  <div>
+    {{ t('hello') }}
+  </div>
+
+  <select v-model="locale">
+    <option value="zh">中文</option>
+    <option value="en">en</option>
+  </select>
 </template>
 
 <script setup lang="ts">
 import { registerSW } from 'virtual:pwa-register'
-import FullScreen from '@/components/Themes/FullScreen.vue'
+import { useI18n } from 'vue-i18n'
+
+const { locale, t } = useI18n()
 
 onMounted(() => {
   registerSW({
