5628a0059d08794f78fba3c8501ec3eab5a8a164
独断万古丶荒 <411496621@qq.com>
2025-02-21 11:06:23 +0800

    基础表格组件进行中 1-2 6:20

---
+++ src/pages/components/table.vue
@@ -0,0 +1,14 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script setup lang="ts">
+definePage({
+  meta: {
+    title: 'pages.tables',
+    icon: 'mdi:apps'
+  }
+})
+</script>
+
+<style scoped></style>
---
+++ src/pages/components/table/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-table> </v-table>
+</template>
+
+<script setup lang="ts">
+definePage({
+  meta: {
+    title: 'pages.table-basic',
+    icon: 'mdi:message'
+  }
+})
+import VTable from '@/components/Table/VTable.vue'
+</script>
+
+<style scoped></style>
