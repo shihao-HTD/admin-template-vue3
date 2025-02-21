6859dcedbf46688ebaf01eee8e3a6be5c7c7fe61
独断万古丶荒 <411496621@qq.com>
2025-02-21 10:36:17 +0800

    完成6-6 完成登录注册的表单校验和自定义校验

--- src/components/Form/LoginForm.vue
+++ src/components/Form/LoginForm.vue
@@ -1,20 +1,31 @@
 <template>
   <!-- form -->
-  <el-form :model="form" class="min-w-[450px]">
-    <el-form-item>
+  <el-form ref="formRef" :rules="rules" :model="form" class="min-w-[450px]">
+    <el-form-item prop="username">
       <el-input
         v-model="form.username"
         :placeholder="$t('pages.login.username')"
         :prefix-icon="getIcon('ep:user')"
       />
     </el-form-item>
-    <el-form-item>
+    <el-form-item prop="password">
       <el-input
+        type="password"
         v-model="form.password"
         :placeholder="$t('pages.login.password')"
         :prefix-icon="getIcon('ep:lock')"
       />
     </el-form-item>
+
+    <el-form-item prop="repassword">
+      <el-input
+        type="password"
+        v-model="form.repassword"
+        :placeholder="$t('pages.login.password')"
+        :prefix-icon="getIcon('ep:lock')"
+      />
+    </el-form-item>
+
     <el-row class="items-center justify-between">
       <el-form-item>
         <el-checkbox :label="$t('pages.login.remember')" v-model="form.remember" name="type" />
@@ -48,6 +59,7 @@
 <script setup lang="tsx">
 import Iconify from '@/components/Icon/Iconify.vue'
 import type { LoginFormProps, LoginItems } from '@/components/Form/type'
+import type { FormInstance, FormRules } from 'element-plus'
 function getIcon(icon: string) {
   return () => <Iconify icon={icon} />
 }
@@ -58,7 +70,19 @@ withDefaults(defineProps<LoginFormProps>(), {
 
 const emits = defineEmits(['clickIcon', 'submit'])
 
-const form = reactive({
+interface LoginFormInterface {
+  username: string
+  password: string
+  repassword: string
+  phone: string | number
+  email: string
+  code: string
+  remember: boolean
+}
+
+const formRef = ref<FormInstance>()
+
+const form = reactive<LoginFormInterface>({
   username: '',
   password: '',
   repassword: '',
@@ -68,8 +92,71 @@ const form = reactive({
   remember: false
 })
 
-function onSubmit() {
-  emits('submit', form)
+const validatePass = useDebounceFn((rule: any, value: any, callback: any) => {
+  if (value.trim() === '') {
+    callback(new Error('请输入密码'))
+  } else if (value !== form.repassword) {
+    callback('两次输入密码不一致')
+  } else {
+    if (!formRef.value) {
+      return
+    }
+    formRef.value.validateField('repassword')
+    callback()
+  }
+}, 200)
+const validatePass2 = useDebounceFn((rule: any, value: any, callback: any) => {
+  if (value.trim() === '') {
+    callback(new Error('请输入再次密码'))
+  } else if (value !== form.password) {
+    callback(new Error('两次输入密码不一致'))
+  } else {
+    if (!formRef.value) {
+      return
+    }
+    formRef.value.validateField('password')
+    callback()
+  }
+}, 300)
+
+const rules = ref<FormRules<LoginFormInterface>>({
+  username: [
+    { required: true, message: '用户名未设置' },
+    {
+      min: 6,
+      max: 16,
+      message: '用户名格式不正确'
+    }
+  ],
+  password: [
+    {
+      min: 6,
+      max: 32,
+      message: '请输入密码'
+    },
+    {
+      asyncValidator: validatePass,
+      trigger: ['change', 'blur']
+    }
+  ],
+  repassword: [
+    {
+      min: 6,
+      max: 32,
+      message: '请输入密码'
+    },
+    {
+      asyncValidator: validatePass2,
+      trigger: ['change', 'blur']
+    }
+  ]
+})
+
+async function onSubmit() {
+  if (formRef.value) {
+    await formRef.value.validate()
+    emits('submit', form)
+  }
 }
 
 function handleClickItem(item: LoginItems) {
