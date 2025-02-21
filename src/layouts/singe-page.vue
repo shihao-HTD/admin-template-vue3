43629a8148a9e6b65bba0bf14cf6e22cb841a68a
独断万古丶荒 <411496621@qq.com>
2025-02-21 09:29:43 +0800

    背景图片响应式布局layout优化

--- src/layouts/singe-page.vue
+++ src/layouts/singe-page.vue
@@ -1,39 +1,94 @@
 <template>
   <div class="w-screen h-screen overflow-hidden">
-    <div class="pos-fixed right-10 top-0">
+    <div class="position-fixed right-10 top-0">
       <el-row class="items-center">
-        <DarkModeToggle class="mr-2"> </DarkModeToggle>
-        <ChangeLocale @change="handleLocalesChange" class="mr-2" :locales="locales"></ChangeLocale>
+        <DarkModeToggle class="mr-2"></DarkModeToggle>
+        <ChangeLocale :locales="locales" @change="handleLocalesChange" class="mr-2"></ChangeLocale>
       </el-row>
     </div>
-
     <div
-      v-if="bg"
-      class="bg-center bg-cover w-full lg:w-1/3 xl:w-1/2 h-screen position-absolute left-0 top-0 z--1"
-      :style="{ backgroundImage: `url(${bg})` }"
+      v-if="settings.bg"
+      :class="[
+        'bg-center bg-cover w-full lg:w-1/3 xl:w-1/2 h-screen position-absolute top-0 z--1',
+        settings.position === 'right' ? 'left-0' : 'right-0',
+        settings.position === 'center' ? 'w-full!' : ''
+      ]"
+      :style="bgStyle"
+      ref="bgRef"
     ></div>
-    <div class="flex flex-col justify-center items-center h-full mx-60">
-      <router-view></router-view>
+    <div class="flex flex-col items-center justify-center h-screen" :style="marginStyle">
+      <div :class="['self-center bg-white rounded p-4', formClass]" ref="formRef">
+        <!-- title -->
+        <div class="text-2xl mb-10">{{ settings.title }}</div>
+        <router-view></router-view>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import ChangeLocale from '@/components/Themes/ChangeLocale.vue'
-import DarkModeToggle from '@/components/Themes/DarkModeToggle.vue'
 import { loadLocaleMessages } from '@/modules/i18n'
 
 const route = useRoute()
-const bg = ref('')
+
+const settings = reactive({
+  bg: '',
+  title: '欢迎登录',
+  position: '',
+  imageWidth: 0
+})
+const bgRef = ref()
+const formRef = ref()
+const margin = ref()
+
+useResizeObserver(bgRef, (entries) => {
+  const { width } = entries[0].contentRect
+  let changeWidth = 0
+  if (formRef.value) {
+    changeWidth = formRef.value.clientWidth
+  }
+  margin.value = (document.body.clientWidth - changeWidth - width) / 2
+})
+
 onBeforeMount(() => {
-  if (route && route.meta && route.meta.backgroundImage) {
-    bg.value = route.meta.backgroundImage as string
+  settings.bg = route?.meta?.backgroundImage as string
+  settings.title = route?.meta?.title as string
+  settings.position = route?.meta?.position as string
+})
+
+const formClass = computed(() => {
+  let cls = ''
+  if (settings.position === 'center') {
+    cls = ''
+  }
+  if (settings.position === 'left') {
+    cls = 'lg:self-start'
+  }
+  if (settings.position === 'right') {
+    cls = 'lg:self-end lt-xl2:shadow'
+  }
+  return cls
+})
+
+const marginStyle = computed(() => {
+  if (margin.value < 0) {
+    return { margin: '0 auto' }
+  }
+  if (settings.position === 'left') {
+    return { marginLeft: margin.value + 'px' }
+  }
+  if (settings.position === 'right') {
+    return { marginRight: margin.value + 'px' }
   }
 })
 
-function handleLocalesChange(locale: string) {
-  loadLocaleMessages(locale)
-}
+const bgStyle = computed(() => {
+  if (settings.imageWidth > 0) {
+    return { width: settings.imageWidth + 'px', backgroundImage: `url(${settings.bg})` }
+  }
+  return { backgroundImage: `url(${settings.bg})` }
+})
+
 const locales = ref([
   {
     text: '中文',
@@ -46,6 +101,10 @@ const locales = ref([
     icon: 'ri:english-input'
   }
 ])
+
+const handleLocalesChange = (locales: string) => {
+  loadLocaleMessages(locales)
+}
 </script>
 
 <style scoped></style>
--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,37 +1,34 @@
 <template>
-  <div class="self-center lg:self-end bg-white lt-xl2:shadow rounded p-4">
-    <h3 class="mb-5">{{ title }}</h3>
-    <el-form :model="form" class="min-w-[450px]">
+  <!-- form -->
+  <el-form :model="form" class="min-w-[450px]">
+    <el-form-item>
+      <el-input
+        v-model="form.username"
+        :placeholder="$t('pages.login.username')"
+        :prefix-icon="getIcon('ep:user')"
+      />
+    </el-form-item>
+    <el-form-item>
+      <el-input
+        v-model="form.password"
+        :placeholder="$t('pages.login.password')"
+        :prefix-icon="getIcon('ep:lock')"
+      />
+    </el-form-item>
+    <el-row class="items-center justify-between">
       <el-form-item>
-        <el-input
-          v-model="form.username"
-          :placeholder="$t('pages.login.username')"
-          :prefix-icon="getIcon('ep:user')"
-        />
+        <el-checkbox :label="$t('pages.login.remember')" v-model="form.remember" name="type" />
       </el-form-item>
-
-      <el-form-item>
-        <el-input
-          v-model="form.password"
-          :placeholder="$t('pages.login.password')"
-          :prefix-icon="getIcon('ep:lock')"
-        />
-      </el-form-item>
-
-      <el-row class="items-center justify-between">
-        <el-form-item :label="$t('pages.login.remember')">
-          <el-checkbox v-model="form.remember" name="type"></el-checkbox>
-        </el-form-item>
-        <el-link type="primary">{{ $t('pages.login.sign-up') }}</el-link>
-      </el-row>
-
-      <el-form-item>
-        <el-button class="w-full mt-4" @click="onSubmit" type="primary">{{
-          $t('pages.login.submit')
-        }}</el-button>
-      </el-form-item>
-    </el-form>
-  </div>
+      <el-link type="primary">{{ $t('pages.login.sign-up') }}</el-link>
+    </el-row>
+    <el-form-item>
+      <!-- 登录 -->
+      <el-button type="primary" @click="onSubmit" class="w-full mt-4">{{
+        $t('pages.login.submit')
+      }}</el-button>
+    </el-form-item>
+  </el-form>
+  <!-- 第三方登录 -->
 </template>
 
 <script setup lang="tsx">
@@ -43,12 +40,13 @@ function getIcon(icon: string) {
 }
 definePage({
   meta: {
+    title: '登录注册',
     layout: 'singe-page',
     meta: '登录注册页',
     hideMenu: true,
-    position: 'left',
-    // backgroundImage:'url'
+    position: 'right',
     // backgroundImage: 'https://cdn.pixabay.com/photo/2022/01/20/17/51/office-desk-6952919_1280.jpg'
+    // backgroundImage:'url'
   }
 })
 
