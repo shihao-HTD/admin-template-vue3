715571b5368aaf23d045cd76e672d432bc084103
独断万古、荒 <411496621@qq.com>
2025-02-25 21:37:20 +0800

    完成2-3 面包屑组件 transition动画效果

--- src/components/Themes/Breadcrumb.vue
+++ src/components/Themes/Breadcrumb.vue
@@ -1,26 +1,60 @@
 <template>
   <el-breadcrumb :separator-icon="ArrowRight" class="ml-4">
-    <el-breadcrumb-item
-      :to="{ path: item.name }"
-      v-for="(item, index) in breadcrumbData"
-      :key="item.fullPath || index"
-    >
-      <span>{{ $t(item.meta.title) }}</span>
-    </el-breadcrumb-item>
+    <transition-group name="breadcrumb" :css="false" @enter="onEnter">
+      <el-breadcrumb-item
+        v-for="(item, index) in breadcrumbData"
+        :data-index="index"
+        :to="{ path: item.name }"
+        :key="item.name"
+      >
+        <span>{{ $t(item.meta.title) }}</span>
+      </el-breadcrumb-item>
+    </transition-group>
   </el-breadcrumb>
 </template>
 
 <script setup lang="tsx">
+import gsap from 'gsap'
+
 const ArrowRight = <i class="i-ep:arrow-right"></i>
 const route = useRoute()
 
-
-
 const breadcrumbData = ref<any[]>([])
+
+const change = ref(-1)
+
+watch(breadcrumbData, (newVal, oldVal) => {
+  change.value = 0
+  for (let i = 0; i < Math.max(newVal.length, oldVal.length); i++) {
+    if (newVal.length < i || oldVal.length < i || newVal[i] !== oldVal[i]) {
+      change.value++
+    }
+  }
+})
 
 function getBreadcrumbData() {
   breadcrumbData.value = route.matched.filter((item) => item.name && item.meta && item.meta.title)
   console.log('=>(Breadcrumb.vue:23) breadcrumbData', breadcrumbData)
+}
+function onEnter(el, done) {
+  let index = el.dataset.index
+  // 子菜单之间进行切换
+  if (change.value === 1) {
+    index = 1
+  }
+  gsap.fromTo(
+    el,
+    {
+      opacity: 0,
+      x: 20
+    },
+    {
+      opacity: 1,
+      x: 0,
+      delay: index * 0.15,
+      onComplete: done
+    }
+  )
 }
 
 watch(
@@ -34,4 +68,18 @@ watch(
 )
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.breadcrumb-move,
+.breadcrumb-entry-active,
+.breadcrumb-leave-active {
+  transition: all 0.5s ease;
+}
+.breadcrumb-entry-from,
+.breadcrumb-leave-to {
+  opacity: 0;
+  transform: translateX(20px);
+}
+.breadcrumb-leave-active {
+  position: absolute;
+}
+</style>
