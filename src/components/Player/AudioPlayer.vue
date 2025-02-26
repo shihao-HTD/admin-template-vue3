89631094551e9a8213f35980709d98bbb41faa53
独断万古、荒 <411496621@qq.com>
2025-02-26 21:43:39 +0800

    完成 6-12 音量控制&循环播放&速率控制交互设计

--- src/components/Player/AudioPlayer.vue
+++ src/components/Player/AudioPlayer.vue
@@ -30,7 +30,6 @@
         <div class="flex-1 flex items-center mx-2 lt-sm:(my-2)">
           <!-- 播放时间 -->
           <span class="text-sm lt-sm:(hidden)">00:00</span>
-
           <ProgressBar v-model="progress"></ProgressBar>
 
           <!-- 播放总体时间 -->
@@ -39,12 +38,26 @@
         <i class="i-iconoir:forward-15-seconds lt-sm:(text-2xl)"></i>
       </div>
       <div class="flex items-center mx-2 lt-sm:(w-full mx-0 my-2 justify-between) lt-sm:(hidden)">
-        <!-- 静音&音量调节按钮 -->
-        <i class="i-clarity:volume-mute-line mr-3"></i>
+        <i class="flex items-center group">
+          <!-- 静音&音量调节按钮 -->
+          <ProgressBar
+            v-model="progress"
+            class="transition-width invisible w-0 mx-0! group-hover:(visible w-[80px] mx-2!)"
+          ></ProgressBar>
+          <i class="i-clarity:volume-mute-line mr-3" v-if="isMute" @click="toggleVolume"></i>
+          <i class="i-clarity:volume-up-line mr-3" v-else @click="toggleVolume"></i>
+        </i>
         <!-- 速率控制按钮 -->
-        <span class="mr-3 cursor-pointer">1x</span>
+        <span class="mr-3 cursor-pointer min-w-8 text-center select-none" @click="handleRateChange"
+          >{{ rateList[rateCurrent] }}x</span
+        >
         <!-- 循环控制按钮 -->
-        <i class="i-bi:sort-down-alt"></i>
+        <span @click="handleLoopChange" class="flex items-center">
+          <i class="i-bi:sort-down-alt" v-if="loop === 0"></i>
+          <i class="i-cil:loop" v-else-if="loop === 1"></i>
+          <i class="i-cil:loop-1" v-else-if="loop === 2"></i>
+          <i class="i-ps:random" v-else></i>
+        </span>
       </div>
     </div>
   </div>
@@ -55,18 +68,41 @@ import type { AudioPlayerProps } from './types'
 
 defineProps<AudioPlayerProps>()
 
-
 const progress = ref(0)
+const rateList = ref([0.5, 1, 1.5, 2])
+const rateCurrent = ref(1)
 
+const loop = ref(0)
 
+const [isMute, toggle] = useToggle(false)
 
+const toggleVolume = () => {
+  toggle()
+}
 
-
-
+const handleRateChange = () => {
+  rateCurrent.value++
+  if (rateCurrent.value >= rateList.value.length) {
+    rateCurrent.value = 0
+  }
+}
+const handleLoopChange = () => {
+  loop.value++
+  if (loop.value >= 4) {
+    loop.value = 0
+  }
+}
 </script>
 
 <style scoped lang="scss">
 i {
   @apply cursor-pointer;
+}
+
+:deep(.el-dropdown-menu__item) {
+  &.active {
+    color: var(--el-dropdown-menuItem-hover-color);
+    background-color: var(--el-dropdown-menuItem-hover-fill);
+  }
 }
 </style>
