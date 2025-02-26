3418dad65f9e768234f708b985ee0ba379ec43f4
独断万古丶荒 <411496621@qq.com>
2025-02-26 15:31:29 +0800

    完成 6-3 自定义封装ECharts组件及响应式调整

--- src/components/Charts/Charts.vue
+++ src/components/Charts/Charts.vue
@@ -1,5 +1,5 @@
 <template>
-  <div ref="chartRef"></div>
+  <div ref="chartRef" :style="chartStyle"></div>
 </template>
 
 <script setup lang="ts">
@@ -7,20 +7,79 @@ import type { EChartsOption, ECharts } from 'echarts'
 import * as echarts from 'echarts'
 
 interface ChartsProps {
-  options: EChartsOption
+  options: Object
   width?: number | string
   height?: number | string
   autoresize?: boolean
 }
-const props = defineProps<ChartsProps>()
 
-const chartRef = ref()
-const chartInstance = ref<ECharts>()
+const props = withDefaults(defineProps<ChartsProps>(), {
+  autoresize: true
+})
+const attrs = useAttrs()
+
+const chartRef = shallowRef()
+const chartInstance = shallowRef<ECharts>()
+
+const emits = defineEmits(['init'])
+
+const chartStyle = computed(() => {
+  // 直接设置到charts组件上的style属性
+  const style = attrs.style || {}
+  const width = formatDimension(props.width, '100%')
+  const height = formatDimension(props.height, '400px')
+  return {
+    width,
+    height,
+    ...style
+  }
+})
+
+watch(
+  () => props.options,
+  (newOptions) => {
+    chartInstance.value?.setOption(newOptions as EChartsOption)
+  },
+  {
+    deep: true
+  }
+)
+
+const fn = useThrottleFn(resize, 50)
+
+onMounted(() => {
+  initChart()
+
+  if (props.autoresize) {
+    window.addEventListener('resize', fn)
+  }
+})
+
+onBeforeUnmount(() => {
+  chartInstance.value && chartInstance.value?.dispose()
+  if (props.autoresize) {
+    window.removeEventListener('resize', fn)
+  }
+})
+
+function formatDimension(value: string | number | undefined, defaultValue: string) {
+  if (typeof value === 'number') {
+    return `${value}px`
+  }
+  return value || defaultValue
+}
 
 function initChart() {
   if (chartRef.value) {
     chartInstance.value = echarts.init(chartRef.value)
-    chartInstance.value.setOption(props.options)
+    chartInstance.value.setOption(props.options as EChartsOption)
+    emits('init', chartInstance.value)
+  }
+}
+
+function resize() {
+  if (chartInstance.value) {
+    chartInstance.value.resize()
   }
 }
 </script>
