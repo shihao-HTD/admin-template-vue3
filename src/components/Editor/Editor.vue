9ab3dcb3e62ff94b5285d73870751f81dc0cca75
独断万古丶荒 <411496621@qq.com>
2025-02-26 16:06:08 +0800

    6-5 进行中 9:40

--- src/components/Editor/Editor.vue
+++ src/components/Editor/Editor.vue
@@ -5,16 +5,42 @@
 <script setup lang="ts">
 import 'vditor/dist/index.css'
 import Vditor from 'vditor'
+import type { EditorOptions, EditorProps } from '@/components/Editor/types'
+
+const props = withDefaults(defineProps<EditorProps>(), {})
 
 const editorRef = ref()
 
+const defaultOptions: EditorOptions = {
+  rtl: false,
+  mode: 'ir',
+  value: '',
+  debugger: false,
+  typewriterMode: false,
+  height: 'auto',
+  minHeight: 400,
+  width: 'auto',
+  placeholder: '',
+  fullscreen: { index: 90 },
+  counter: {
+    enable: false, // 默认值: false
+    type: 'markdown' // 默认值: 'markdown'
+  },
+  link: {
+    isOpen: true // 默认值: true
+  },
+  image: {
+    isPreview: true // 默认值: true
+  },
+  cache: { enable: true, id: Math.random().toString(16).slice(2) },
+  lang: 'zh_CN',
+  theme: 'classic',
+  icon: 'ant',
+  cdn: 'https://unpkg.com/vditor@3.9.6'
+}
+
 onMounted(() => {
-  new Vditor(editorRef.value, {
-    mode: 'sv',
-    cache: {
-      id: Math.random().toString(36).slice(2)
-    }
-  })
+  new Vditor(editorRef.value, Object.assign(defaultOptions, props.options))
 })
 </script>
 
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,6 @@
 <template>
   <div>
-    <Editor></Editor>
-
-
+    <Editor :options="{}"></Editor>
   </div>
 </template>
 
